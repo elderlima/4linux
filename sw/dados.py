cbc224d0ec7350c57dbfaac068f9c27157bcf5e5
elderlima <emaildoelder@gmail.com>
2021-07-14 22:22:06 -0300

    commit

--- sw/dados.py
+++ sw/dados.py
@@ -24,21 +24,55 @@ def setup():
 
     if db.naves.count() > 0:
         return
+    
+    if db.personagens.count() > 0:
+        return
+
+    if db.veiculos.count() > 0:
+        return
 
     db.naves.insert_many(naves)
-    db.personafens.insert_many(personagens)
+    db.personagens.insert_many(personagens)
     db.veiculos.insert_many(veiculos)
 
 def naves():
     db = cliente.sw
     return db.naves.find()
 
+def personagens():
+    db = cliente.sw
+    return db.personagens.find()
+
+def veiculos():
+    db = cliente.sw
+    return db.veiculos.find()
+
 def criar_naves(nave):
     db = cliente.sw
     return db.naves.insert_one(nave)
+
+def criar_personagens(personagem):
+    db = cliente.sw
+    return db.personagens.insert_one(personagem)
+
+def criar_veiculos(veiculo):
+    db = cliente.sw
+    return db.veiculos.insert_one(veiculo)
 
 def modificar_naves(oid, nave):
     return cliente.sw.naves.update_one(
         oid,
         {"$set": nave}
+    )
+
+def modificar_personagem(oid, personagem):
+    return cliente.sw.personagens.update_one(
+        oid,
+        {"$set": personagem}
+    )
+
+def modificar_veiculo(oid, veiculo):
+    return cliente.sw.veiculos.update_one(
+        oid,
+        {"$set": veiculo}
     )
--- sw/personagens.py
+++ sw/personagens.py
@@ -1,4 +1,6 @@
 import flask
+from bson.json_util import dumps
+from pymongo import results
 import sw.dados
 
 # bp = flask.Flask(__name__)
@@ -6,21 +8,44 @@ bp = flask.Blueprint("personagens", __name__, url_prefix="/personagens")
 
 @bp.route("")
 def listar_personagens():
-    return flask.jsonify(sw.dados.personagens)
+    personagem = dumps(list(sw.dados.personagens()))
+    return flask.Response(personagem, headers=sw.dados.cabecalhos)
+    # return flask.jsonify(sw.dados.personagens)
 
 @bp.route("<int:id>")
 def get_personagens(id):
-    return flask.jsonify(sw.dados.personagens[id])
+    personagem = dumps(list(sw.dados.personagens())[id])
+    return flask.Response(personagem, headers=sw.dados.cabecalhos)
+    # return flask.jsonify(sw.dados.personagens[id])
+
+# @bp.route("", methods=["POST"])
+# def criar_personagem():
+#     personagem = flask.request.json
+#     sw.dados.personagens.append(personagem)
+#     id = len(sw.dados.personagens) - 1
+#     return flask.jsonify({"id":id})
 
 @bp.route("", methods=["POST"])
 def criar_personagem():
     personagem = flask.request.json
-    sw.dados.personagens.append(personagem)
-    id = len(sw.dados.personagens) - 1
-    return flask.jsonify({"id":id})
+    result = sw.dados.criar_personagens(personagem)
+    return flask.jsonify({"id": str(result.inserted_id)})
 
-@bp.route("<int:id>", methods=["PUT"])
+# @bp.route("<int:id>", methods=["PUT"])
+# def modificar_personagem(id):
+#     personagem = flask.request.json
+#     sw.dados.personagens[id] = personagem
+#     return flask.jsonify({"ok": True})
+
+@bp.route("/<int:id>", methods=["PUT"])
 def modificar_personagem(id):
     personagem = flask.request.json
-    sw.dados.personagens[id] = personagem
-    return flask.jsonify({"ok": True})
+    personagens = list(sw.dados.personagens())
+    personagem_velha = personagens[id]
+    result = sw.dados.modificar_personagem(
+        {"_id": personagem_velha["_id"]},
+        personagem
+    )
+    return flask.jsonify({
+            "modificationsa": result.modified_count
+    })
